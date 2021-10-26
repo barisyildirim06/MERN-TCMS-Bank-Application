2b424e0d6fead3a3f6c8bf1898ad3e716f8830e0
barisyilirim06 <yildrmbaris@gmail.com>
2021-10-26 19:12:33 +0300

    Move controller files from router/user to controllers/user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,4 +1,24 @@
 let { User } = require('../models/User');
+const multer = require('multer');
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'client/public/static/img/')
+    },
+    filename: (req, file, cb) => {
+        cb(null, `${Date.now()}_${file.originalname}`)
+    },
+    fileFilter: (req, file, cb) => {
+        const ext = path.extname(file.originalname)
+        if (ext !== '.jpg' || ext !== '.png') {
+            return cb(res.status(400).end('only jpg, png are allowed'), false);
+        }
+        cb(null, true)
+    }
+})
+
+var upload = multer({ storage: storage }).single("file")
+
 
 module.exports = {
     userUpdate(req,res,next){
@@ -92,5 +112,82 @@ module.exports = {
         User.findOneAndUpdate({ _id: req.body._id }, { verified: 1 }, () => {
             return res.status(200).json({ success: true, message: "User successfully verified" })
         })
+    },
+    loginUser (req, res) {
+        User.findOne({ email: req.body.email }, (err, user) => {
+            if (!user)
+                return res.json({
+                    loginSuccess: false,
+                    message: "Auth failed, email not found"
+                });
+    
+            user.comparePassword(req.body.password, (err, isMatch) => {
+                if (!isMatch)
+                    return res.json({ loginSuccess: false, message: "Wrong password" });
+    
+                user.generateToken((err, user) => {
+                    if (err) return res.status(400).send(err);
+                    res.cookie("w_authExp", user.tokenExp);
+                    res.cookie("w_auth", user.token)
+                    .status(200)
+                    .json({
+                        loginSuccess: true, userId: user._id, isAdmin: user.isAdmin? true : false
+                    });
+                });
+            });
+        });
+    },
+    registerUser (req, res) {
+
+        const user = new User(req.body);
+    
+        user.save((err, doc) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).json({
+                success: true
+            });
+        });
+    },
+    authUser (req, res) {
+        res.status(200).json({
+            _id: req.user._id,
+            isAuth: true,
+            isAdmin: req.user.isAdmin? true : false,
+            email: req.user.email,
+            phone: req.user.phone,
+            companyName: req.user.companyName,
+            irdNumber: req.user.irdNumber,
+            companyOfficeNumber: req.user.companyOfficeNumber,
+            name: req.user.name,
+            lastname: req.user.lastname,
+            verified: req.user.verified,
+            image: req.user.image,
+            agentFirstName: req.user.agentFirstName,
+            agentLastName: req.user.agentLastName,
+            agentJobTitle: req.user.agentJobTitle,
+            nzdWithdrawalAccount: req.user.nzdWithdrawalAccount,
+            usdWithdrawalAccount: req.user.usdWithdrawalAccount,
+            audWithdrawalAccount: req.user.audWithdrawalAccount,
+            idimage: req.user.idimage,
+            userID: req.user.userID,
+        });
+    },
+    uploadImage (req, res) {
+
+        upload(req, res, err => {
+            if (err) {
+                return res.json({ success: false, err })
+            }
+            return res.json({ success: true, image: res.req.file.path, fileName: res.req.file.filename })
+        })
+    
+    },
+    logoutUser (req, res) {
+        User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
+            if (err) return res.json({ success: false, err });
+            return res.status(200).send({
+                success: true
+            });
+        });
     }
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,113 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { User } = require("../models/User");
-const { userUpdate, listUsers, resetPassword, verifyUser } = require('../controllers/user.js')
+const { userUpdate, listUsers, resetPassword, verifyUser, loginUser, registerUser, authUser, uploadImage, logoutUser } = require('../controllers/user.js')
 const { auth } = require("../middleware/auth");
-const multer = require('multer');
-
-var storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'client/public/static/img/')
-    },
-    filename: (req, file, cb) => {
-        cb(null, `${Date.now()}_${file.originalname}`)
-    },
-    fileFilter: (req, file, cb) => {
-        const ext = path.extname(file.originalname)
-        if (ext !== '.jpg' || ext !== '.png') {
-            return cb(res.status(400).end('only jpg, png are allowed'), false);
-        }
-        cb(null, true)
-    }
-})
-
-var upload = multer({ storage: storage }).single("file")
 
 //=================================
 //             User
 //=================================
 
-router.post("/uploadImage", auth, (req, res) => {
+router.post("/uploadImage", auth, uploadImage);
 
-    upload(req, res, err => {
-        if (err) {
-            return res.json({ success: false, err })
-        }
-        return res.json({ success: true, image: res.req.file.path, fileName: res.req.file.filename })
-    })
+router.get("/auth", auth, authUser);
 
-});
+router.post("/register", registerUser);
 
-router.get("/auth", auth, (req, res) => {
-    res.status(200).json({
-        _id: req.user._id,
-        isAuth: true,
-        isAdmin: req.user.isAdmin? true : false,
-        email: req.user.email,
-        phone: req.user.phone,
-        companyName: req.user.companyName,
-        irdNumber: req.user.irdNumber,
-        companyOfficeNumber: req.user.companyOfficeNumber,
-        name: req.user.name,
-        lastname: req.user.lastname,
-        verified: req.user.verified,
-        image: req.user.image,
-        agentFirstName: req.user.agentFirstName,
-        agentLastName: req.user.agentLastName,
-        agentJobTitle: req.user.agentJobTitle,
-        nzdWithdrawalAccount: req.user.nzdWithdrawalAccount,
-        usdWithdrawalAccount: req.user.usdWithdrawalAccount,
-        audWithdrawalAccount: req.user.audWithdrawalAccount,
-        idimage: req.user.idimage,
-        userID: req.user.userID,
-    });
-});
+router.post("/login", loginUser);
 
-router.post("/register", (req, res) => {
-
-    const user = new User(req.body);
-
-    user.save((err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).json({
-            success: true
-        });
-    });
-});
-
-router.post("/login", (req, res) => {
-    User.findOne({ email: req.body.email }, (err, user) => {
-        if (!user)
-            return res.json({
-                loginSuccess: false,
-                message: "Auth failed, email not found"
-            });
-
-        user.comparePassword(req.body.password, (err, isMatch) => {
-            if (!isMatch)
-                return res.json({ loginSuccess: false, message: "Wrong password" });
-
-            user.generateToken((err, user) => {
-                if (err) return res.status(400).send(err);
-                res.cookie("w_authExp", user.tokenExp);
-                res.cookie("w_auth", user.token)
-                .status(200)
-                .json({
-                    loginSuccess: true, userId: user._id, isAdmin: user.isAdmin? true : false
-                });
-            });
-        });
-    });
-});
-
-router.get("/logout", auth, (req, res) => {
-    User.findOneAndUpdate({ _id: req.user._id }, { token: "", tokenExp: "" }, (err, doc) => {
-        if (err) return res.json({ success: false, err });
-        return res.status(200).send({
-            success: true
-        });
-    });
-});
+router.get("/logout", auth, logoutUser);
 
 router.post('/update/:id',userUpdate);
 router.post('/reset-password', resetPassword);
