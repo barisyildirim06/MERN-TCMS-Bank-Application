8ef5d8a0f55dd8f6b1e55ee002e765e74a132c03
barisyildirim06 <yildrmbaris@gmail.com>
2021-10-31 19:03:33 +0300

    Fix transfer create

--- server/controllers/transfer.js
+++ server/controllers/transfer.js
@@ -1,31 +1,46 @@
 let { Transfer } = require('../models/Transfer');
 let { User } = require('../models/User');
 let { GeneralLedger } = require('../models/GeneralLedger');
+let { PendingWithdrawal } = require('../models/PendingWithdrawal');
 
-module.exports = {
-    async transferCreate(req, res) {
-        //save all the data we got from the client into the DB 
-        const donor = req.user;
-        const recipient = await User.findOne({ userID: req.body.accountID })
-        if (!recipient) return res.status(200).json({ success: false, message: 'Please provide a valid accountID' })
-        if (donor._id === recipient._id || donor.userID === recipient.userID) {
-            console.log(donor._id)
-            console.log(recipient._id)
+const transferCreate = async (req, res) => {
+    const donorAccount = req.user;
+        const recipientAccount = await User.findOne({ userID: req.body.accountID })
+        if (!recipientAccount) return res.status(200).json({ success: false, message: 'Please provide a valid accountID' })
+        if (donorAccount._id === recipientAccount._id || donorAccount.userID === recipientAccount.userID) {
             return res.status(200).json({ success: false, message: 'Users cannot transfer money to their own accounts' })
         }
 
-        const ledger = await GeneralLedger.find({ transactionNotes : donor._id })
-        // const transferData = {
-        //     ...req.body,
-        //     recipientAccount: recipient._id,
-        //     recipientUserID: recipient.userID,
-        //     donorAccount: donor._id,
-        //     donorUserID: donor.userID,
-        // }
-        // const transfer = new Transfer(transferData)
-        // transfer.save((err) => {
-        //     if (err) return res.status(200).json({ success: false, message: 'An error occured during transfer. Please try it later' })
-        //     return res.status(200).json({ success: true, message: 'The transfer done successfully' })
-        // })
-    },
+        const general = await GeneralLedger.find({ transactionNotes: req.user._id });
+        const withdrawal = await PendingWithdrawal.find({ account: req.user._id });
+        const recipient = await Transfer.find({ recipientAccount: req.user._id });
+        const donor = await Transfer.find({ donorAccount: req.user._id });
+    
+        let account = {
+            principal: general?.filter(el => el.transactionType === 'CREDIT' && el.currency === req.body.currency )?.length? general?.filter(el => el.transactionType === 'CREDIT' && el.currency === req.body.currency )?.reduce((a, b) => ({amount: a?.amount + b?.amount}))?.amount : 0,
+            interestAcrrued: general?.filter(el => el.transactionType === 'ACRUED INTEREST' && el.currency === req.body.currency )?.length ? general?.filter(el => el.transactionType === 'ACRUED INTEREST' && el.currency === req.body.currency ).reduce((a, b) => ({amount: a?.amount + b?.amount}))?.amount : 0,
+            fees: general?.filter(el => el.transactionType === 'FEES' && el.currency === req.body.currency )?.length? general?.filter(el => el.transactionType === 'FEES' && el.currency === req.body.currency )?.reduce((a, b) => ({amount: a?.amount + b?.amount}))?.amount : 0,
+            withdrawls: general?.filter(el => el.transactionType === 'DEBIT' && el.currency === req.body.currency )?.length? general?.filter(el => el.transactionType === 'DEBIT' && el.currency === req.body.currency )?.reduce((a, b) => ({amount: a?.amount + b?.amount}))?.amount * -1 : 0,
+            pendingWithdrawls: withdrawal?.filter(el => el.status === 'Pending' && el.currency === req.body.currency)?.length? withdrawal?.filter(el => el.status === 'Pending' && el.currency === req.body.currency)?.reduce((a, b) => ({amount: a?.amount + b?.amount}))?.amount : 0,
+            recipientTransfers: recipient?.filter(el => el.currency === req.body.currency)?.length? recipient?.filter(el => el.currency === req.body.currency)?.reduce((a, b) => ({amount: a?.amount + b?.amount}))?.amount : 0,
+            donorTransfers: donor?.filter(el => el.currency === req.body.currency)?.length? donor?.filter(el => el.currency === req.body.currency)?.reduce((a, b) => ({amount: a?.amount + b?.amount}))?.amount  * -1 : 0,
+        }
+        const availableBalance = account.principal + account.interestAcrrued + account.fees + account.withdrawls + account.pendingWithdrawls + account.recipientTransfers + account.donorTransfers
+        if (req.body.amount > availableBalance) return res.status(200).json({ success: false, message: 'Transfer amount cannot be larger than available balance' })
+        const transferData = {
+            ...req.body,
+            recipientAccount: recipient._id,
+            recipientUserID: recipient.userID,
+            donorAccount: donor._id,
+            donorUserID: donor.userID,
+        }
+        const transfer = new Transfer(transferData)
+        transfer.save((err) => {
+            if (err) return res.status(200).json({ success: false, message: 'An error occured during transfer. Please try it later' })
+            return res.status(200).json({ success: true, message: 'The transfer done successfully' })
+        })
+}
+
+module.exports = {
+    transferCreate
 }
