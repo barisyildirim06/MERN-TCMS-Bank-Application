91222257af9b2c16f36397ee1ffb4cf3f3bc29fc
barisyildirim06 <yildrmbaris@gmail.com>
2021-11-07 16:11:33 +0300

    Add downloadCsv function to utils

--- client/src/utils.js
+++ client/src/utils.js
@@ -8,4 +8,36 @@ export class Utils {
         style: 'currency',
         currency: 'USD',
     });
+
+    static downloadCsv(data, fileName) {
+        const objectToCsv = (data) => {
+            let csvRows = [];
+            const headers = Object.keys(data[0]);
+            csvRows.push(headers.join(','));
+    
+            for (const row of data) {
+                const values = headers.map(header => {
+                    const escaped = (''+row[header]).replace(/"/g, '\\"');
+                    return `"${escaped}"`
+                })
+                csvRows.push(values.join(','));
+            }
+            return csvRows.join('\n');
+        }
+        const csvData = objectToCsv(data);
+    
+        const download = (csv) => {
+            const blob = new Blob([csv], { type: 'text/csv' });
+            const url = window.URL.createObjectURL(blob);
+            const a = document.createElement('a');
+            a.setAttribute('hidden', '');
+            a.setAttribute('href', url);
+            a.setAttribute("download", fileName);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+        return download(csvData)
+
+    }
 }
