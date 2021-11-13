4780a2b34ba91484ab9671ed32a307f408a9f921
barisyildirim06 <yildrmbaris@gmail.com>
2021-11-13 22:00:27 +0300

    Change available balance & create new ledger after interest rate save

--- server/controllers/interestRate.js
+++ server/controllers/interestRate.js
@@ -1,4 +1,6 @@
 const { InterestRate } = require("../models/InterestRate");
+const { User } = require("../models/User");
+const { GeneralLedger } = require("../models/GeneralLedger");
 
 module.exports = {
     async interestRateCreate(req, res) {
@@ -8,7 +10,7 @@ module.exports = {
 
         await interestRate.save(async (err) => {
             if (err) return res.status(400).json({ success: false, err })
-            const interest = await InterestRate.findOne({ _id: interestRate?._id}).exec()
+            const interest = await InterestRate.findOne({ _id: interestRate?._id }).exec()
             return res.status(200).json({ success: true, message: "New Interest Rate is created", interest })
         })
 
@@ -16,7 +18,7 @@ module.exports = {
     interestRateList(req, res) {
         if (!req.user.isAdmin) return res.status(400).json({ success: false, message: "You don't have access" })
         const sevenDaysUnix = new Date().setDate((new Date()).getDate() - 7);
-        InterestRate.find({ createdAt : { $gte : sevenDaysUnix } })
+        InterestRate.find({ createdAt: { $gte: sevenDaysUnix } })
             .then(interestRates => {
                 return res.json(interestRates)
             })
@@ -24,8 +26,22 @@ module.exports = {
     },
     async interestRateUpdate(req, res) {
         if (!req.user.isAdmin) return res.status(400).json({ success: false, message: "You don't have access" })
-        await InterestRate.findOneAndUpdate({ currency: req.body.currency }, { rate: req.body.rate })
-        await InterestRate.findOneAndUpdate({ currency: 'lastWeek' + req.body.currency }, { rate: req.body.rate, confirmAmount: req.body.confirmAmount })
+        const { confirmAmount, currency, rate } = req.body;
+        await InterestRate.findOneAndUpdate({ currency: currency }, { rate: rate })
+        await InterestRate.findOneAndUpdate({ currency: 'lastWeek' + currency }, { rate: rate, confirmAmount: confirmAmount })
+        const users = await User.find();
+        const totalAmount = await users.reduce((a, b) => ({ availableBalance: a.availableBalance + b.availableBalance })).availableBalance;
+
+        users.forEach(async user => {
+            const { _id, availableBalance } = user;
+            const calculatedReturn = confirmAmount * (availableBalance / totalAmount)
+            await User.findByIdAndUpdate({ _id: _id }, { availableBalance: Number(availableBalance) + Number(calculatedReturn) })
+            const general = { currency, transactionNotes: _id, transactionDate: new Date().toISOString().split('T')[0], amount: calculatedReturn, transactionType: 'ACCRUED INTEREST' }
+            const generalLedger = new GeneralLedger(general)
+            await generalLedger.save((err) => {
+                if (err) return res.status(400).json({ success: false, err })
+            })
+        })
         return res.status(200).json({ success: true, message: "Interest Rate is updated" });
     }
 }
