117820565d1e747f11ca56cd6637d197bd5fd79c
barisyildirim06 <yildrmbaris@gmail.com>
2021-11-14 23:36:48 +0300

    Add space between checkbox items

--- client/src/dialogs/confirm-dialog/confirm-dialog-visible.js
+++ client/src/dialogs/confirm-dialog/confirm-dialog-visible.js
@@ -50,35 +50,35 @@ function ConfirmDialog({ onClose, visible, setValues, onConfirm, values }) {
         <Dialog visible={visible} width={1000} onClose={handleClose}>
             <div className='accountCardTop' style={{ height: '80vh', width: '100%', margin: '1vh', overflowY: 'scroll' }}>
                 <p className='depositText' style={{ lineHeight: '30px' }}>If you meet the criteria for one or more of the categories below, you are considered to be a wholesale investor</p>
-                <Checkbox checked={checkboxValues[0]} onChange={() => handleCheckboxValueChange(0)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[0]} onChange={() => handleCheckboxValueChange(0)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     1. You're an "eligible investor" because you have sufficient knowledge and experience dealing in financial products that enables you to assess the merits and risks of the transaction. Eligible investors are required to certify that they are an eligible investor by completing the TREASURY CAPITAL
                     MANAGEMENT SERVICES <a style={{ color: '#1890ff' }} href='static/img/investor-certificate.pdf' target='_blank'>Eligible Investor Certificate NZ</a> and having it certified by an independent lawyer, accountant, or financial adviser (unless you meet one of the other criteria below).
                 </Checkbox>
-                <Checkbox checked={checkboxValues[1]} onChange={() => handleCheckboxValueChange(1)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[1]} onChange={() => handleCheckboxValueChange(1)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     2. Your net assets, combined with the assets of entities you control, exceeded $5 million for the 2 most recent financial years, or the total turnover of you and the entities you control exceeded $5 million for the 2 most recent financial years.
                 </Checkbox>
-                <Checkbox checked={checkboxValues[2]} onChange={() => handleCheckboxValueChange(2)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[2]} onChange={() => handleCheckboxValueChange(2)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     3. You own, or at some time during the last 2 years have owned, a portfolio of "financial products" of a value of at least $1 million. Financial products include debt securities, equity securities, managed investment products, and derivatives.
                 </Checkbox>
-                <Checkbox checked={checkboxValues[3]} onChange={() => handleCheckboxValueChange(3)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[3]} onChange={() => handleCheckboxValueChange(3)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     4. You have, during the last 2 years, carried out 1 or more transactions to acquire "financial products" where the amount payable under those transactions (in aggregate) is at least $1 million and the other parties to the transactions were not associated with you.
                 </Checkbox>
-                <Checkbox checked={checkboxValues[4]} onChange={() => handleCheckboxValueChange(4)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[4]} onChange={() => handleCheckboxValueChange(4)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     5. Your principal business is investing in financial products.
                 </Checkbox>
-                <Checkbox checked={checkboxValues[5]} onChange={() => handleCheckboxValueChange(5)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[5]} onChange={() => handleCheckboxValueChange(5)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     6. Your principal business is providing a financial adviser service in relation to financial products.
                 </Checkbox>
-                <Checkbox checked={checkboxValues[6]} onChange={() => handleCheckboxValueChange(6)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[6]} onChange={() => handleCheckboxValueChange(6)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     7. You're in the business of trading in financial products on behalf of other people.
                 </Checkbox>
-                <Checkbox checked={checkboxValues[7]} onChange={() => handleCheckboxValueChange(7)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[7]} onChange={() => handleCheckboxValueChange(7)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     8. Within the last 10 years you've been employed in an investment business and, for at least 2 years during that 10 year period, you participated in the investment decisions made by that investment business.
                 </Checkbox>
-                <Checkbox checked={checkboxValues[8]} onChange={() => handleCheckboxValueChange(8)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[8]} onChange={() => handleCheckboxValueChange(8)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     9. You are a financial adviser in New Zealand and hold, or operate under, a Financial Advice Provider (FAP) licence.
                 </Checkbox>
-                <Checkbox checked={checkboxValues[9]} onChange={() => handleCheckboxValueChange(9)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px' }}>
+                <Checkbox checked={checkboxValues[9]} onChange={() => handleCheckboxValueChange(9)} style={{ display: 'flex', alignItems: 'baseline', fontSize: '20px', margin: '15px 0' }}>
                     10. You're investing $750,000 or more into the company.
                 </Checkbox>
                 <br />
